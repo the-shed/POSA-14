ef70909fa7050753e4b70de491236b1ab8db19de
Gary <gary@deborah.home>
2014-07-07 18:15:25 +0100

    W7-A6 submitted solution

--- assignments/week-7-assignment-6/iRemember/src/edu/vuum/mocca/ui/LoginActivity.java
+++ assignments/week-7-assignment-6/iRemember/src/edu/vuum/mocca/ui/LoginActivity.java
@@ -26,7 +26,7 @@ public class LoginActivity extends StoryActivityBase{
 	EditText mPassword;
 	
 	// Make sure we use maximum security to store login credentials
-	static final int MAX_SECURITY = Integer.MAX_VALUE;
+	static final int MAX_SECURITY = 1;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
